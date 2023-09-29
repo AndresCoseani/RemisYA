ce1b512ead5f758b3c168886bac5c83d387d7802
root <mgiraudo@ies21.edu.ar>
2023-09-29 12:48:44 -0300

    Home

---
+++ RemisYA/PRINCIPAL.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace RemisYA
+{
+    partial class PRINCIPAL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tAREASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fORM1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fORM2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fORM3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fORM4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fORM3ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fORM3ToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fORM3ToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fORM3ToolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fORM3ToolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fORM3ToolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tAREASToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(203, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tAREASToolStripMenuItem
+            // 
+            this.tAREASToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fORM1ToolStripMenuItem,
+            this.fORM2ToolStripMenuItem,
+            this.fORM3ToolStripMenuItem,
+            this.fORM4ToolStripMenuItem,
+            this.fORM3ToolStripMenuItem1,
+            this.fORM3ToolStripMenuItem2,
+            this.fORM3ToolStripMenuItem3,
+            this.fORM3ToolStripMenuItem4,
+            this.fORM3ToolStripMenuItem5,
+            this.fORM3ToolStripMenuItem6});
+            this.tAREASToolStripMenuItem.Name = "tAREASToolStripMenuItem";
+            this.tAREASToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.tAREASToolStripMenuItem.Text = "TAREAS";
+            // 
+            // fORM1ToolStripMenuItem
+            // 
+            this.fORM1ToolStripMenuItem.Name = "fORM1ToolStripMenuItem";
+            this.fORM1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fORM1ToolStripMenuItem.Text = "FORM 1";
+            // 
+            // fORM2ToolStripMenuItem
+            // 
+            this.fORM2ToolStripMenuItem.Name = "fORM2ToolStripMenuItem";
+            this.fORM2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fORM2ToolStripMenuItem.Text = "FORM 2";
+            // 
+            // fORM3ToolStripMenuItem
+            // 
+            this.fORM3ToolStripMenuItem.Name = "fORM3ToolStripMenuItem";
+            this.fORM3ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fORM3ToolStripMenuItem.Text = "FORM 3";
+            this.fORM3ToolStripMenuItem.Click += new System.EventHandler(this.fORM3ToolStripMenuItem_Click);
+            // 
+            // fORM4ToolStripMenuItem
+            // 
+            this.fORM4ToolStripMenuItem.Name = "fORM4ToolStripMenuItem";
+            this.fORM4ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fORM4ToolStripMenuItem.Text = "FORM 4";
+            // 
+            // fORM3ToolStripMenuItem1
+            // 
+            this.fORM3ToolStripMenuItem1.Name = "fORM3ToolStripMenuItem1";
+            this.fORM3ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.fORM3ToolStripMenuItem1.Text = "FORM 5";
+            // 
+            // fORM3ToolStripMenuItem2
+            // 
+            this.fORM3ToolStripMenuItem2.Name = "fORM3ToolStripMenuItem2";
+            this.fORM3ToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.fORM3ToolStripMenuItem2.Text = "FORM 6";
+            // 
+            // fORM3ToolStripMenuItem3
+            // 
+            this.fORM3ToolStripMenuItem3.Name = "fORM3ToolStripMenuItem3";
+            this.fORM3ToolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.fORM3ToolStripMenuItem3.Text = "FORM 7";
+            // 
+            // fORM3ToolStripMenuItem4
+            // 
+            this.fORM3ToolStripMenuItem4.Name = "fORM3ToolStripMenuItem4";
+            this.fORM3ToolStripMenuItem4.Size = new System.Drawing.Size(180, 22);
+            this.fORM3ToolStripMenuItem4.Text = "FORM 8";
+            // 
+            // fORM3ToolStripMenuItem5
+            // 
+            this.fORM3ToolStripMenuItem5.Name = "fORM3ToolStripMenuItem5";
+            this.fORM3ToolStripMenuItem5.Size = new System.Drawing.Size(180, 22);
+            this.fORM3ToolStripMenuItem5.Text = "FORM 9";
+            this.fORM3ToolStripMenuItem5.Click += new System.EventHandler(this.fORM3ToolStripMenuItem5_Click);
+            // 
+            // fORM3ToolStripMenuItem6
+            // 
+            this.fORM3ToolStripMenuItem6.Name = "fORM3ToolStripMenuItem6";
+            this.fORM3ToolStripMenuItem6.Size = new System.Drawing.Size(180, 22);
+            this.fORM3ToolStripMenuItem6.Text = "FORM 10";
+            // 
+            // PRINCIPAL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(203, 381);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "PRINCIPAL";
+            this.Text = "PRINCIPAL";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tAREASToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fORM1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fORM2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fORM3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fORM4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fORM3ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem fORM3ToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem fORM3ToolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem fORM3ToolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem fORM3ToolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem fORM3ToolStripMenuItem6;
+    }
+}
---
+++ RemisYA/PRINCIPAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemisYA
+{
+    public partial class PRINCIPAL : Form
+    {
+        public PRINCIPAL()
+        {
+            InitializeComponent();
+        }
+
+        private void fORM3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fORM3ToolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
